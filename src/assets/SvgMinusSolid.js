dc6c109c787c1afaa974da7d2e62940d6ad5a8f0
Paulo <paulozamora2000@gmail.com>
2020-03-22 14:35:49 -0600

    Added button to remove a matrix

---
+++ src/assets/SvgMinusSolid.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function SvgMinusSolid(props) {
+  return (
+    <svg aria-hidden="true" data-icon="minus" viewBox="0 0 448 512" {...props}>
+      <path
+        fill="currentColor"
+        d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
+      />
+    </svg>
+  );
+}
+
+export default SvgMinusSolid;
